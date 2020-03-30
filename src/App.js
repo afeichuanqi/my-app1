7ae9ff32f20a24c5fa8f262a108ea251667764b0
JackeyPan <11976964@qq.com>
2020-03-30 19:04:54 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import PictureSelect from './components/PictureSelect';
+
+const App = () => {
+    const [value, setValue] = React.useState(['1']);
+    const pictures = [
+        {
+            id: '1',
+            name: 'foo',
+            url: 'https://gw.alipayobjects.com/mdn/rms_d212b7/afts/img/A*LlfeSa8N0WgAAAAAAAAAAABkARQnAQ'
+        },
+        {
+            id: '2',
+            name: 'foo',
+            url: 'https://gw.alipayobjects.com/mdn/rms_d212b7/afts/img/A*LlfeSa8N0WgAAAAAAAAAAABkARQnAQ'
+        },
+        {
+            id: '3',
+            name: 'foo',
+            url: 'https://gw.alipayobjects.com/mdn/rms_d212b7/afts/img/A*LlfeSa8N0WgAAAAAAAAAAABkARQnAQ'
+        },
+    ]; // 同上面的数据
+
+    // 输出用户选择图片 id。
+    console.log(value);
+    return <PictureSelect pictures={pictures} value={value} onChange={(value) => setValue(value)} />
+};
+
+
+export default App;
---
+++ src/components/PictureSelect.js
@@ -0,0 +1,99 @@
+import React from "react";
+
+export default class PictureSelect extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.checkSet = new Set(props.value);
+    }
+
+    _inputSelectAll = (e) => {
+        const {pictures, onChange} = this.props;
+        const checked = e.target.checked;
+        if (!checked) {
+            onChange([]);
+            this.checkSet.clear();
+            return;
+
+        }
+
+        const value = [];
+        for (let item of pictures) {
+            value.push(item.id);
+            this.checkSet.add(item.id)
+        }
+        onChange(value)
+    }
+
+    render() {
+        const {pictures, value} = this.props;
+        return <>
+            <div style={{display: 'flex', flexDirection: 'row', marginTop: 10, marginBottom: 10, alignItems: 'center'}}>
+                <input checked={value.length === pictures.length} onChange={this._inputSelectAll} type="checkbox"
+                       name="vehicle" value="Bike"/>
+                <div style={{fontSize: 13}}>已选中{value.length}个文件</div>
+            </div>
+            <div style={{
+                display: 'flex',
+                flexDirection: 'row',
+                flexWrap: 'warp'
+            }}>
+                {pictures.map((item, index, arr) => {
+                    const checked = value.indexOf(item.id) !== -1 ? true : false;
+                    return <PictureBox checked={checked} onChange={this.onChange} key={item.id} picData={item}/>
+                })}
+            </div>
+        </>
+    }
+
+    onChange = (id, checked) => {
+        checked ? this.checkSet.add(id) : this.checkSet.delete(id);
+        this.props.onChange([...this.checkSet])
+    }
+}
+
+class PictureBox extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            checked: this.props.checked
+        }
+    }
+
+    onChange = () => {
+        const {picData, onChange,checked} = this.props;
+        onChange(picData.id, !checked)
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.checked !== this.props.checked) {
+            this.setState({
+                checked: nextProps.checked
+            })
+        }
+
+    }
+
+    shouldComponentUpdate(nextProps, nextState, nextContext) {
+
+        return nextState.checked !== this.state.checked
+
+    }
+
+    render() {
+        const {picData} = this.props;
+        const {checked} = this.state;
+
+        return <div style={{position: 'relative', marginRight: 10, width: 120}}>
+            <img style={{width: "100%"}}
+                 src={picData.url}
+            />
+            <input onChange={this.onChange}
+                   checked={checked}
+                   style={{position: 'absolute', top: 0, left: 0,}}
+                   type="checkbox"
+            />
+            <div style={{width: "100%", wordWrap: "break-word", fontSize: 12}}>{picData.name}.jpg</div>
+        </div>
+    }
+}
